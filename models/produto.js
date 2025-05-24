15342208a9b2484416473b735c517d6e52928f81
Pablo Selares <134446830+PabloSelares@users.noreply.github.com>
2025-05-24 16:31:22 -0300

    update

--- models/produto.js
+++ models/produto.js
@@ -5,7 +5,11 @@ const ProdutoSchema = new mongoose.Schema({
   description: String,
   price: Number,
   type: String,
-  image: String
+  image: String,
+   foodtruckId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'FoodTruck'
+  }
 });
 
 module.exports = mongoose.model('Produto', ProdutoSchema);
--- routes/FoodTruck.js
+++ routes/FoodTruck.js
@@ -1,15 +1,32 @@
-
 const express = require('express');
-const router = express.Router();
-const FoodTruck = require('../models/FoodTruck'); 
+const router = express.Router();  // <-- DEFINIÇÃO DO ROUTER
+const FoodTruck = require('../models/FoodTruck');
+const Produto = require('../models//produto');  // não esqueça de importar o modelo Produto
 
-
+// rota para listar todos os food trucks
 router.get('/', async (req, res) => {
   try {
-    const foodTrucks = await FoodTruck.find(); 
+    const foodTrucks = await FoodTruck.find();
     res.json(foodTrucks);
   } catch (err) {
     res.status(500).json({ erro: 'Erro ao buscar Food Trucks' });
+  }
+});
+
+// rota para buscar food truck específico e seus produtos
+router.get('/:id', async (req, res) => {
+  try {
+    const foodTruck = await FoodTruck.findById(req.params.id);
+    if (!foodTruck) {
+      return res.status(404).json({ erro: 'Food Truck não encontrado' });
+    }
+    const produtos = await Produto.find({ foodtruckId: foodTruck._id });
+    res.json({
+      ...foodTruck.toObject(),
+      produtos
+    });
+  } catch (err) {
+    res.status(500).json({ erro: 'Erro ao buscar Food Truck e seus produtos' });
   }
 });
 
